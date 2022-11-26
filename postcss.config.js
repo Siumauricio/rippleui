8a585e1d2d4f8c3b26fc195c78e3700635c232b1
Mauricio <Siumauricio@unitec.edu>
2022-11-26 03:08:05 -0600

    chore: add comment to create minified css files

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-   plugins: {
-      'postcss-import': {},
-      'tailwindcss/nesting': {},
-      'tailwindcss': {},
-      'autoprefixer': {},
-   },
+  plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": {},
+    tailwindcss: {},
+    autoprefixer: {},
+    // ...(process.env.NODE_ENV !== "production" ? { cssnano: {} } : {}), Only for production
+  },
 };
