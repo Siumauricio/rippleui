b260325871c5410606ba978306a9fc4c404ec4ca
Mauricio <Siumauricio@unitec.edu>
2022-11-26 02:13:41 -0600

    feat: add dist source

---
+++ dist/colors/cyan.d.ts
@@ -0,0 +1,24 @@
+export declare const cyan: {
+    50: string;
+    100: string;
+    200: string;
+    300: string;
+    400: string;
+    500: string;
+    600: string;
+    700: string;
+    800: string;
+    900: string;
+};
+export declare const cyanDark: {
+    50: string;
+    100: string;
+    200: string;
+    300: string;
+    400: string;
+    500: string;
+    600: string;
+    700: string;
+    800: string;
+    900: string;
+};
---
+++ dist/colors/green.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greenDark = exports.green = void 0;
+exports.green = {
+    50: "#F1FDF7",
+    100: "#E8FCF1",
+    200: "#DAFBE8",
+    300: "#C8F9DD",
+    400: "#ADF5CC",
+    500: "#88F1B6",
+    600: "#17C964",
+    700: "#13A452",
+    800: "#108944",
+    900: "#06371B",
+};
+exports.greenDark = {
+    50: "#042F14",
+    100: "#06381B",
+    200: "#074A24",
+    300: "#0A6130",
+    400: "#0B7439",
+    500: "#0F9549",
+    600: "#17C964",
+    700: "#41EC8B",
+    800: "#78F2AD",
+    900: "#ECFDF4",
+};
