91dea796bc9738560d315cd08ade027598dd1137
Mauricio <Siumauricio@unitec.edu>
2022-11-27 04:35:03 -0600

    chore: add config for theme colors switcher automatically

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,59 @@
-/** @type {import('tailwindcss').Config} */
-const { colors } = require("./dist/js/index");
 
+const plugin = require("tailwindcss/plugin");
+
+const { theme } = require("./dist/js/index");
+const { varTheme, lighTheme, darkTheme} = theme;
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./components/**/*.{html,js}", "./experiments/**/*.{html,js}"],
+  darkMode: "class",
   theme: {
     extend: {
-      colors: colors,
+      colors: varTheme.colors,
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        [":root"]: {
+          colorScheme: "light",
+          ...lighTheme.colors,
+        },
+      });
+
+      addBase({
+        ["@media (prefers-color-scheme: dark)"]: {
+          [":root"]: {
+            colorScheme: "dark",
+            ...darkTheme.colors,
+          },
+        },
+      });
+
+      addBase({
+        ["[data-theme=light]"]: {
+          colorScheme: "light",
+          ...lighTheme.colors,
+        },
+      });
+
+      addBase({
+        ["[data-theme=dark]"]: {
+          colorScheme: "dark",
+          ...darkTheme.colors,
+        },
+      });  
+      
+      // addBase({
+      //   ["[data-theme=testing]"]: {
+      //     colorScheme: "dark",
+      //     '--primary': 'red',
+      //     // ...darkTheme.colors,
+      //   },
+      // });   
+      // console.log(matchVariant("hover", "hover"));
+      // Add your custom styles here
+    }),
+  ],
 };
