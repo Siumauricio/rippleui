302d2719608e59219d1de47747e06ad7356ded19
Mauricio <Siumauricio@unitec.edu>
2022-11-27 04:36:58 -0600

    refactor: add css variables styles to each color pallete

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,5 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.theme = void 0;
 const ligh_theme_1 = __importDefault(require("./ligh-theme"));
-module.exports = Object.assign({}, ligh_theme_1.default);
+const var_theme_1 = __importDefault(require("./var-theme"));
+const dark_theme_1 = __importDefault(require("./dark-theme"));
+exports.theme = {
+    varTheme: var_theme_1.default,
+    lighTheme: ligh_theme_1.default,
+    darkTheme: dark_theme_1.default
+};
