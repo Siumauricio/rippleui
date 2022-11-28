93d7efa0281a557970771d17cef39adf15447018
Mauricio <Siumauricio@unitec.edu>
2022-11-27 22:10:13 -0600

    refactor: create function to pass all the config to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,59 +1,9 @@
-
-const plugin = require("tailwindcss/plugin");
-
-const { theme } = require("./dist/js/index");
-const { varTheme, lighTheme, darkTheme} = theme;
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./components/**/*.{html,js}", "./experiments/**/*.{html,js}"],
   darkMode: "class",
   theme: {
-    extend: {
-      colors: varTheme.colors,
-    },
+    extend: {},
   },
-  plugins: [
-    plugin(function ({ addBase, theme }) {
-      addBase({
-        [":root"]: {
-          colorScheme: "light",
-          ...lighTheme.colors,
-        },
-      });
-
-      addBase({
-        ["@media (prefers-color-scheme: dark)"]: {
-          [":root"]: {
-            colorScheme: "dark",
-            ...darkTheme.colors,
-          },
-        },
-      });
-
-      addBase({
-        ["[data-theme=light]"]: {
-          colorScheme: "light",
-          ...lighTheme.colors,
-        },
-      });
-
-      addBase({
-        ["[data-theme=dark]"]: {
-          colorScheme: "dark",
-          ...darkTheme.colors,
-        },
-      });  
-      
-      // addBase({
-      //   ["[data-theme=testing]"]: {
-      //     colorScheme: "dark",
-      //     '--primary': 'red',
-      //     // ...darkTheme.colors,
-      //   },
-      // });   
-      // console.log(matchVariant("hover", "hover"));
-      // Add your custom styles here
-    }),
-  ],
+  plugins: [require("./dist/js/index")],
 };
--- theme/index.ts
+++ theme/index.ts
@@ -1,9 +1,98 @@
 import lighTheme from "./ligh-theme";
 import varTheme from "./var-theme";
 import darkTheme from "./dark-theme";
+import plugin from "tailwindcss/plugin";
 
-export const theme = {
-  varTheme,
-  lighTheme,
-  darkTheme
-};
+// const theme = {
+//   varTheme,
+//   lighTheme,
+//   darkTheme,
+// };
+
+export const themePlugin = plugin(
+  ({ addBase, theme }) => {
+    theme("theme.colors", varTheme.colors);
+    addBase({
+      [":root"]: {
+        colorScheme: "light",
+        ...lighTheme.colors,
+      },
+    });
+    addBase({
+      ["@media (prefers-color-scheme: dark)"]: {
+        [":root"]: {
+          colorScheme: "dark",
+          ...darkTheme.colors,
+        },
+      },
+    });
+    addBase({
+      ["[data-theme=light]"]: {
+        colorScheme: "light",
+        ...lighTheme.colors,
+      },
+    });
+    addBase({
+      ["[data-theme=dark]"]: {
+        colorScheme: "dark",
+        ...darkTheme.colors,
+      },
+    });
+  },
+  // Import of Theme variables
+  {
+    theme: {
+      extend: {
+        colors: varTheme.colors,
+      },
+    },
+  }
+);
+
+module.exports = themePlugin;
+
+// export const config = plugin.withOptions(
+//   function (options = {}) {
+//     console.log(options);
+//     return function ({ addBase, ...rest }) {
+//       console.log(rest);
+//       addBase({
+//         [":root"]: {
+//           colorScheme: "light",
+//           ...lighTheme.colors,
+//         },
+//       });
+//       addBase({
+//         ["@media (prefers-color-scheme: dark)"]: {
+//           [":root"]: {
+//             colorScheme: "dark",
+//             ...darkTheme.colors,
+//           },
+//         },
+//       });
+//       addBase({
+//         ["[data-theme=light]"]: {
+//           colorScheme: "light",
+//           ...lighTheme.colors,
+//         },
+//       });
+//       addBase({
+//         ["[data-theme=dark]"]: {
+//           colorScheme: "dark",
+//           ...darkTheme.colors,
+//         },
+//       });
+//     };
+//   },
+//   function (options) {
+//     return {
+//       theme: {
+//         extend: {
+//           colors: varTheme.colors,
+//         },
+//       },
+//     };
+//   }
+// );
+
+// module.exports = config;
