1d1ee2c40b4bbc24f34d234686b049114f14d7d6
Mauricio <Siumauricio@unitec.edu>
2022-11-30 03:21:32 -0600

    refactor: agrupate css var into objects

--- theme/var-theme.ts
+++ theme/var-theme.ts
@@ -1,4 +1,4 @@
-export const blue = {
+ const blue = {
   50: "var(--blue-50)",
   100: "var(--blue-100)",
   200: "var(--blue-200)",
@@ -11,7 +11,7 @@ export const blue = {
   900: "var(--blue-900)",
 };
 
-export const green = {
+ const green = {
   50: "var(--green-50)",
   100: "var(--green-100)",
   200: "var(--green-200)",
@@ -24,7 +24,7 @@ export const green = {
   900: "var(--green-900)",
 };
 
-export const cyan = {
+ const cyan = {
   50: "var(--cyan-50)",
   100: "var(--cyan-100)",
   200: "var(--cyan-200)",
@@ -37,7 +37,7 @@ export const cyan = {
   900: "var(--cyan-900)",
 };
 
-export const gray = {
+ const gray = {
   50: "var(--gray-50)",
   100: "var(--gray-100)",
   200: "var(--gray-200)",
@@ -50,7 +50,7 @@ export const gray = {
   900: "var(--gray-900)",
 };
 
-export const pink = {
+ const pink = {
   50: "var(--pink-50)",
   100: "var(--pink-100)",
   200: "var(--pink-200)",
@@ -63,7 +63,7 @@ export const pink = {
   900: "var(--pink-900)",
 };
 
-export const purple = {
+ const purple = {
   50: "var(--purple-50)",
   100: "var(--purple-100)",
   200: "var(--purple-200)",
@@ -76,7 +76,7 @@ export const purple = {
   900: "var(--purple-900)",
 };
 
-export const yellow = {
+ const yellow = {
   50: "var(--yellow-50)",
   100: "var(--yellow-100)",
   200: "var(--yellow-200)",
@@ -89,7 +89,7 @@ export const yellow = {
   900: "var(--yellow-900)",
 };
 
-export const red = {
+ const red = {
   50: "var(--red-50)",
   100: "var(--red-100)",
   200: "var(--red-200)",
@@ -102,30 +102,40 @@ export const red = {
   900: "var(--red-900)",
 };
 
+const background = {
+  backgroundPrimary : "var(--backgroundPrimary)",
+  backgroundSecondary : "var(--backgroundSecondary)",
+  border: "var(--border)",
+}
+
+const text = {
+  content1: "var(--content1)",
+  content2: "var(--content2)",
+  content3: "var(--content3)",
+  whiteInverted: "var(--whiteInverted)",
+}
+
+const baseColors = {
+  neutral: "var(--neutral)",
+  primary: "var(--primary)",
+  secondary: "var(--secondary)",
+  success: "var(--success)",
+  error: "var(--error)",
+  warning: "var(--warning)",
+}
+
 export default {
   colors: {
     // background colors
-    backgroundPrimary : "var(--backgroundPrimary)",
-    backgroundSecondary : "var(--backgroundSecondary)",
-    border: "var(--border)",
-
-    background: "var(--background)",
-    backgroundAlpha: "var(--backgroundAlpha)",
-    foreground: "#000",
+    ...background,
 
     // Text
-    content1: "var(--content1)",
-    content2: "var(--content2)",
-    content3: "var(--content3)",
+    ...text,
 
     // Primary colors
-    neutral: "var(--neutral)",
-    primary: "var(--primary)",
-    secondary: "var(--secondary)",
-    success: "var(--success)",
-    error: "var(--error)",
-    warning: "var(--warning)",
+    ...baseColors,
 
+    //semantic colors
     blue,
     purple,
     green,
@@ -134,10 +144,5 @@ export default {
     cyan,
     pink,
     gray,
-
-    // misc
-    whiteInverted: "var(--whiteInverted)",
-
-    // border: "rgba(0, 0, 0, 0.15)",
   },
 };
