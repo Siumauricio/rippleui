3feeccb9d25e7659a0417912b3074d9380e04666
Mauricio <Siumauricio@unitec.edu>
2022-11-30 03:23:07 -0600

    refactor: inject base, componentes and utilities into plugin config

--- theme/index.ts
+++ theme/index.ts
@@ -6,40 +6,46 @@ import fs from "fs";
 import postcss from "postcss";
 import postcssJs from "postcss-js";
 
-const readCSS = fs.readFileSync("./dist/css/styles.css", "utf-8");
+const baseCSS = fs.readFileSync("./dist/css/base.css", "utf-8");
+const componentsCSS = fs.readFileSync("./dist/css/components.css", "utf-8");
+const utilitiesCSS = fs.readFileSync("./dist/css/utilities.css", "utf-8");
 
-export const themePlugin = plugin(
-  ({ addBase, theme, config }) => {
-    const root = postcss.parse(readCSS);
-    const cssObject = postcssJs.objectify(root);
-    // console.log(config("rippleui"));
-    // addBase(postcssJs.objectify(root));
-    addBase({
+
+export const config = plugin(
+  ({ addBase, addComponents, addUtilities, theme, config}) => {
+
+    const base = postcss.parse(baseCSS);
+    const components = postcss.parse(componentsCSS);
+    const utilities = postcss.parse(utilitiesCSS);
+    const baseObj = postcssJs.objectify(base);
+    const componentsObj = postcssJs.objectify(components);
+    const utilitiesObj = postcssJs.objectify(utilities);
+
+    // Add base styles/colors
+    addBase([{
+      ...baseObj,
       [":root"]: {
         colorScheme: "light",
         ...lighTheme.colors,
       },
-    });
-    addBase({
       ["@media (prefers-color-scheme: dark)"]: {
         [":root"]: {
           colorScheme: "dark",
           ...darkTheme.colors,
         },
       },
-    });
-    addBase({
       ["[data-theme=light]"]: {
         colorScheme: "light",
         ...lighTheme.colors,
       },
-    });
-    addBase({
       ["[data-theme=dark]"]: {
         colorScheme: "dark",
         ...darkTheme.colors,
       },
-    });
+    }]);
+   
+    addComponents(componentsObj);
+    addUtilities(utilitiesObj);
   },
   // Import of Theme variables
   {
@@ -51,4 +57,4 @@ export const themePlugin = plugin(
   }
 );
 
-module.exports = themePlugin;
+module.exports = config;
