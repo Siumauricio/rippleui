5cf18529670e5afb383acda16350d0b462b79ff7
Mauricio <Siumauricio@unitec.edu>
2022-11-30 03:20:28 -0600

    feat: add tailwind config

---
+++ docs/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("./dist/js/index.js")
+  ],
+}
