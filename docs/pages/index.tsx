9b0e28d0d034a8a8e41829eb3dbe6b189c209cc0
Mauricio <Siumauricio@unitec.edu>
2022-12-03 04:27:02 -0600

    chore: add rippleui package

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,14 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div >
-     <button className='btn top-7'>
-      Click me
-     </button>
-     <input type="text" className='input top-7  content-auto '/>
+    <div>
+      <button className="btn btn-primary top-7">Click me</button>
+      <input type="text" className="input top-7  content-auto " />
     </div>
-  )
+  );
 }
--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -7,7 +7,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [
-    require("./dist/js/index.js")
-  ],
-}
+  plugins: [require("rippleui")],
+};
