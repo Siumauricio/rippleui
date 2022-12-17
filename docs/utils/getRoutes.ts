081f4a5d6d2d4e12187316fa4b86acc851d9ef04
Mauricio <Siumauricio@unitec.edu>
2022-12-16 20:41:29 -0600

    refactor: removed layout dir

--- docs/utils/getRoutes.ts
+++ docs/utils/getRoutes.ts
@@ -1,25 +1,24 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
-type Folder = 'get-started' | 'components' | 'layout';
+type Folder = "get-started" | "components";
 
 export type Folders = {
-   [key in Folder]: string;
+  [key in Folder]: string;
 };
 
 export const getRoutes = () => {
-   const folders: Folders = {
-      'components': path.join(process.cwd(), 'content/components'),
-      'get-started': path.join(process.cwd(), 'content/get-started'),
-      'layout': path.join(process.cwd(), 'content/layout'),
-   };
+  const folders: Folders = {
+    components: path.join(process.cwd(), "content/components"),
+    "get-started": path.join(process.cwd(), "content/get-started"),
+  };
 
-   const routes = Object.keys(folders).map((folder: string) => {
-      const files = fs.readdirSync(folders[folder as Folder]);
-      return files.map((file) => {
-         const name = file.replace('.mdx', '');
-         return `/docs/${folder}/${name}`;
-      });
-   });
-   return routes.flat();
+  const routes = Object.keys(folders).map((folder: string) => {
+    const files = fs.readdirSync(folders[folder as Folder]);
+    return files.map((file) => {
+      const name = file.replace(".mdx", "");
+      return `/docs/${folder}/${name}`;
+    });
+  });
+  return routes.flat();
 };
