18313bd58d79897bde8d3bc503deae94df3c6df2
Mauricio <Siumauricio@unitec.edu>
2022-12-19 02:57:37 -0600

    feat(docs): add new icons

---
+++ docs/components/icons/Book.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const BookIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6 22H21V20H6.012C5.55 19.988 5 19.805 5 19C5 18.195 5.55 18.012 6.012 18H19H20H21V17V15V4C21 2.897 20.103 2 19 2H6C4.794 2 3 2.799 3 5V8V14V17V19C3 21.201 4.794 22 6 22ZM5 8V5C5 4.195 5.55 4.012 6 4H19V15V16H5V14V8Z"
+        className="fill-content3"
+      />
+      <path d="M8 6H17V8H8V6Z" className="fill-content3" />
+    </svg>
+  );
+};
