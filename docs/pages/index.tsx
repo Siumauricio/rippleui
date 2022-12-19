aea8234ae66cc050fd6b8099f49aad85bd6a10c5
Mauricio <Siumauricio@unitec.edu>
2022-12-19 03:00:05 -0600

    refactor(docs): add content of initial page

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,9 +1,18 @@
 import { ReactElement } from "react";
 import { MainLayout } from "../components/layout/MainLayout";
+import { Community } from "../components/styles/Community";
+import { Hero } from "../components/styles/Hero";
 import { Navbar } from "../components/styles/Navbar";
+import { Reusable } from "../components/styles/Reusable";
 
 export default function Home() {
-  return <>Init PAGE</>;
+  return (
+    <div className="flex flex-col gap-6 pb-28">
+      <Hero />
+      <Reusable />
+      <Community />
+    </div>
+  );
 }
 
 Home.getLayout = function getLayout(page: ReactElement) {
