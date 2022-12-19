f4c521e8fd94d575f3c8ad03f6b034caf1c5b731
Mauricio <Siumauricio@unitec.edu>
2022-12-19 02:57:59 -0600

    refactor(docs): removed padding and overflow to main layout

--- docs/components/layout/MainLayout.tsx
+++ docs/components/layout/MainLayout.tsx
@@ -15,7 +15,7 @@ export const MainLayout = ({ children, headings }: Props) => {
       {/* <TopNav /> */}
       <div className="sticky top-16 z-[2] flex h-screen w-full flex-grow">
         <div className="hidden md:block">{/* <SideNav /> */}</div>
-        <main className="top-16 h-full w-full flex-grow overflow-hidden text-base">
+        <main className="top-16 h-full w-full flex-grow  p-[4.5rem_0rem_2.5rem] text-base lg:p-[7rem_0rem_2rem]">
           {children}
         </main>
 
