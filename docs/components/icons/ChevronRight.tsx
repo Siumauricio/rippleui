b4d8514b76c8bffde41b0fd0f4d3909889225ba1
Mauricio <Siumauricio@unitec.edu>
2022-12-19 18:38:36 -0600

    refactor(docs): refactor icons color

---
+++ docs/components/icons/ChevronRight.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const ChevronRight = () => {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M7.37999 21.0098C7.86999 21.4998 8.65999 21.4998 9.14999 21.0098L17.46 12.6998C17.85 12.3098 17.85 11.6798 17.46 11.2898L9.14999 2.9798C8.65999 2.4898 7.86999 2.4898 7.37999 2.9798C6.88999 3.4698 6.88999 4.2598 7.37999 4.7498L14.62 11.9998L7.36999 19.2498C6.88999 19.7298 6.88999 20.5298 7.37999 21.0098Z"
+        fill="var(--content3)"
+      />
+    </svg>
+  );
+};
