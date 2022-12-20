370fecd04ac4ede3d7986527d2099f8fee862c00
Mauricio <Siumauricio@unitec.edu>
2022-12-19 18:42:00 -0600

    refactor(docs): add meta tag

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,6 +1,8 @@
 import { ReactElement } from "react";
 import { MainLayout } from "../components/layout/MainLayout";
+import Meta from "../components/mdx/layout/Meta";
 import { Community } from "../components/styles/Community";
+import { DarkMode } from "../components/styles/DarkMode";
 import { Hero } from "../components/styles/Hero";
 import { Navbar } from "../components/styles/Navbar";
 import { Reusable } from "../components/styles/Reusable";
@@ -10,6 +12,7 @@ export default function Home() {
     <div className="flex flex-col gap-6 pb-28">
       <Hero />
       <Reusable />
+      <DarkMode />
       <Community />
     </div>
   );
@@ -18,6 +21,13 @@ export default function Home() {
 Home.getLayout = function getLayout(page: ReactElement) {
   return (
     <MainLayout>
+      <Meta
+        meta={{
+          title: "Ripple UI | TailwindCSS Components",
+          description:
+            "Ripple UI is a collection of TailwindCSS components that are reusable and easy to use.",
+        }}
+      />
       <Navbar />
       {page}
     </MainLayout>
