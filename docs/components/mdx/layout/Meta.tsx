ead9afc38e25cdfccfd4479db4548a123f3248fd
Mauricio <Siumauricio@unitec.edu>
2022-12-20 23:51:04 -0600

    refactor(docs): add next seo package

--- docs/components/mdx/layout/Meta.tsx
+++ docs/components/mdx/layout/Meta.tsx
@@ -1,39 +1,49 @@
-import React, {useMemo} from 'react';
-import Head from 'next/head';
+import React, { useMemo } from "react";
+import Head from "next/head";
+import { NextSeo } from "next-seo";
+import { URL_WEB } from "../../../lib/constant";
 
 export interface Meta {
-   title: string;
-   description: string;
+  title: string;
+  description: string;
 }
 
 export interface LayoutProps {
-   meta: Meta;
-   getStaticProps?: any;
+  meta: Meta;
+  getStaticProps?: any;
 }
 
 const toCapitalize = (name: string) => {
-   // @ts-ignore
-   const [first, ...rest] = name;
-   return `${first.toUpperCase()}${rest.join('')}`;
+  // @ts-ignore
+  const [first, ...rest] = name;
+  return `${first.toUpperCase()}${rest.join("")}`;
 };
 
 const Meta: React.FC<React.PropsWithChildren<LayoutProps>> = React.memo(
-   ({meta}) => {
-      const capitalizeTitle = useMemo(() => {
-         if (!meta.title) return '';
-         if (meta.title.toLowerCase().startsWith('use'))
-            return `${meta.title} - `;
-         return `${toCapitalize(meta.title)} |  Ripple UI`;
-      }, [meta.title]);
-      return (
-         <Head>
-            <title>{capitalizeTitle}</title>
-            <meta name="title" content={capitalizeTitle} />
-            <meta name="description" content={meta.description} />
-         </Head>
-      );
-   }
+  ({ meta }) => {
+    const capitalizeTitle = useMemo(() => {
+      if (!meta.title) return "";
+      if (meta.title.toLowerCase().startsWith("use")) return `${meta.title} - `;
+      return `${toCapitalize(meta.title)} RippleUI | TailwindCSS Component`;
+    }, [meta.title]);
+    return (
+      <NextSeo
+        title={capitalizeTitle}
+        description={meta.description}
+        themeColor="#000000"
+        canonical={URL_WEB}
+        openGraph={{
+          images: [
+            {
+              url: "logo.png",
+              alt: "RippleUI Logo",
+            },
+          ],
+        }}
+      />
+    );
+  }
 );
 
-Meta.displayName = 'Layout';
+Meta.displayName = "Layout";
 export default Meta;
