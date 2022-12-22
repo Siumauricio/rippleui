7c5dfeb6e51387c46bc0c037a9ba302817ffcde7
Mauricio <Siumauricio@unitec.edu>
2022-12-22 00:19:04 -0600

    refactor(docs): add twitter cards to meta and favicon ico

--- docs/components/mdx/layout/Meta.tsx
+++ docs/components/mdx/layout/Meta.tsx
@@ -33,6 +33,11 @@ const Meta: React.FC<React.PropsWithChildren<LayoutProps>> = React.memo(
         description={meta.description}
         themeColor="#000000"
         canonical={URL_WEB + router.asPath}
+        twitter={{
+          handle: "@",
+          site: "@",
+          cardType: "summary_large_image",
+        }}
         openGraph={{
           images: [
             {
@@ -41,6 +46,34 @@ const Meta: React.FC<React.PropsWithChildren<LayoutProps>> = React.memo(
             },
           ],
         }}
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: URL_WEB + "/favicon.ico",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: URL_WEB + "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+          {
+            rel: "icon",
+            href: URL_WEB + "/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            rel: "icon",
+            href: URL_WEB + "/favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
+          {
+            rel: "manifest",
+            href: URL_WEB + "/site.webmanifest",
+          },
+        ]}
       />
     );
   }
