0ea74226c8f6e5195555a706ca1ab9ac1dab4f1d
Mauricio <Siumauricio@unitec.edu>
2022-12-26 07:40:07 -0600

    refactor(docs): change var to tailwind class

--- docs/components/icons/MenuIcon.tsx
+++ docs/components/icons/MenuIcon.tsx
@@ -11,7 +11,7 @@ export const MenuIcon = () => {
     >
       <path
         d="M4 6H20V8H4V6ZM4 11H20V13H4V11ZM4 16H20V18H4V16Z"
-        fill="var(--content3)"
+        className="fill-content3"
       ></path>
     </svg>
   );
