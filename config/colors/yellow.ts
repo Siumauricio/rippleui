9ec61744c856546205558afc0ce2b386174d6630
Mauricio <Siumauricio@unitec.edu>
2022-12-26 07:32:27 -0600

    refactor(config): convert hexa to rgb

--- config/colors/yellow.ts
+++ config/colors/yellow.ts
@@ -1,27 +1,28 @@
 import { PalleteVariables } from "../types";
+import { hexToRGB } from "../utils/hexToRGB";
 
 export const yellow: PalleteVariables<"yellow"> = {
-  "--yellow-50": "#FEF9F0",
-  "--yellow-100": "#FEF5E7",
-  "--yellow-200": "#FDEFD8",
-  "--yellow-300": "#FCE7C5",
-  "--yellow-400": "#FBDBA7",
-  "--yellow-500": "#F9CB80",
-  "--yellow-600": "#F5A524",
-  "--yellow-700": "#B97509",
-  "--yellow-800": "#925D07",
-  "--yellow-900": "#4E3104",
+  "--yellow-50": "254 249 240",
+  "--yellow-100": "254 245 231",
+  "--yellow-200": "253 239 216",
+  "--yellow-300": "252 231 197",
+  "--yellow-400": "251 219 167",
+  "--yellow-500": "249 203 128",
+  "--yellow-600": "245 165 36",
+  "--yellow-700": "185 117 9",
+  "--yellow-800": "146 93 7",
+  "--yellow-900": "78 49 4",
 };
 
 export const yellowDark: PalleteVariables<"yellow"> = {
-  "--yellow-50": "#3A2503",
-  "--yellow-100": "#442B03",
-  "--yellow-200": "#583804",
-  "--yellow-300": "#704705",
-  "--yellow-400": "#845306",
-  "--yellow-500": "#A66908",
-  "--yellow-600": "#F5A524",
-  "--yellow-700": "#F6AD37",
-  "--yellow-800": "#F8C572",
-  "--yellow-900": "#FEF7EC",
+  "--yellow-50": "58 37 3",
+  "--yellow-100": "68 43 3",
+  "--yellow-200": "88 56 4",
+  "--yellow-300": "112 71 5",
+  "--yellow-400": "132 83 6",
+  "--yellow-500": "166 105 8",
+  "--yellow-600": "245 165 36",
+  "--yellow-700": "246 173 55",
+  "--yellow-800": "248 197 114",
+  "--yellow-900": "254 247 236",
 };
