e3a5910ac2b39491d6a6b3d4ec050360abc710e4
Mauricio <Siumauricio@unitec.edu>
2022-12-29 06:40:10 -0600

    feat(config): types for rippleui theme config

---
+++ config/types/config.types.ts
@@ -0,0 +1,30 @@
+import { Theme } from "./theme.types";
+
+type MakeOptional<Type, Key extends keyof Type> = Omit<Type, Key> &
+  Partial<Pick<Type, Key>>;
+
+type ThemeExtended = MakeOptional<Theme, "colors" | "prefersColorScheme">;
+
+/**
+ * The @{config} object.
+ * @typedef {Type} ConfigTheme
+ * @property {boolean} [defaultStyle] - Will apply by default bg-color and color to the root element.
+ * @property {string[]} [removeThemes] - The list of themes to remove.
+ * @property {ThemeExtended[]} [themes] -  The list of themes to modify, add or remove, you can also add new variables in each theme.
+ */
+type ConfigTheme = {
+  /**
+   * Will apply by default bg-color and color to the root element `(default: true)`
+   */
+  defaultStyle?: boolean;
+  /**
+   * The list of themes to remove `(default: [])`
+   */
+  removeThemes?: string[];
+  /**
+   * The list of themes to modify, add you can also add new variables in each theme.
+   */
+  themes?: ThemeExtended[];
+};
+
+export type Config = ConfigTheme;
