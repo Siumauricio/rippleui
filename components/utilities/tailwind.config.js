e3aa62baf625c983e74ef59f51f1944889173634
Mauricio <Siumauricio@unitec.edu>
2023-01-06 22:45:23 -0600

    refactor: separate utilities to another config

---
+++ components/utilities/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
