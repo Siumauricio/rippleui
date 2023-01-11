66264944acc5b8e643f9ef1a1889c3faa072ab29
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2023-01-11 13:37:54 -0600

    refactor(docs): reduced size

--- docs/components/icons/CopyIcon.tsx
+++ docs/components/icons/CopyIcon.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export const CopyIcon = () => {
   return (
     <svg
-      width="24"
-      height="24"
+      width="20"
+      height="20"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
