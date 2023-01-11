e93b897230c0d87b4920c87eb25dc69b56561e6d
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2023-01-11 13:40:21 -0600

    refactor(docs): disableTabs flag added

--- docs/components/styles/DarkModeToggle.tsx
+++ docs/components/styles/DarkModeToggle.tsx
@@ -19,8 +19,8 @@ export const DarkModeToggle = () => {
       className="cursor-pointer"
       onClick={() => setTheme("light")}
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width="22"
+      height="22"
       viewBox="0 0 24 24"
     >
       <path
@@ -33,8 +33,8 @@ export const DarkModeToggle = () => {
       onClick={() => setTheme("dark")}
       className="cursor-pointer"
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width="22"
+      height="22"
       viewBox="0 0 24 24"
     >
       <path
--- docs/components/styles/GetStarted.tsx
+++ docs/components/styles/GetStarted.tsx
@@ -5,14 +5,14 @@ import { CodeBlock } from "../playground/CodeBlock";
 
 export const GetStarted = () => {
   return (
-    <section className="w-full  overflow-hidden bg-backgroundSecondary py-28 px-4 md:gap-20 xl:px-0">
+    <section className="bg-backgroundSecondary  w-full overflow-hidden py-28 px-4 md:gap-20 xl:px-0">
       <div className="flex flex-col items-center gap-4 pb-5">
         <div className="w-full ">
           <h2 className="mx-auto w-full text-center text-3xl font-bold md:text-4xl">
             Get Started Any Way You Want
           </h2>
         </div>
-        <p className=" mx-auto w-full max-w-xl text-center text-base text-content3">
+        <p className=" text-content3 mx-auto w-full max-w-xl text-center text-base">
           Get started with RippleUI quickly by using the CDN, installing it
           through a package manager, or downloading the source code.
         </p>
@@ -26,16 +26,17 @@ export const GetStarted = () => {
           </div>
           <div>
             <CodeBlock
+              disableTabs
               iconClass="sm:max-w-md top-10"
               blockClass="sm:max-w-md"
               language="bash"
             >{`npm install rippleui`}</CodeBlock>
           </div>
-          <p className="mx-auto w-full max-w-lg  pt-10 text-content2">
+          <p className="text-content2 mx-auto w-full  max-w-lg pt-10">
             Read our{" "}
             <Link
               href={"/docs/get-started/installation"}
-              className="cursor-pointer text-primary"
+              className="text-primary cursor-pointer"
             >
               installation
             </Link>{" "}
@@ -51,13 +52,14 @@ export const GetStarted = () => {
           </div>
           <div className="flex w-full flex-col ">
             <CodeBlock
+              disableTabs
               disabledFormat
               iconClass="sm:max-w-md top-10"
               blockClass="sm:max-w-md"
               language="html"
             >{`<link
-rel="stylesheet"
-href="https://cdn.jsdelivr.net/npm/rippleui@${RippleUIVersion}/dist/css/styles.css"
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/rippleui@${RippleUIVersion}/dist/css/styles.css"
 />`}</CodeBlock>
           </div>
         </div>
