bbf14485ae7ed6384c2f1fc2f67916acecefcb9b
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2023-01-28 00:40:34 -0600

    refactor(docs): add searcher styles

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,12 +1,11 @@
 import "../styles/globals.css";
+import "../styles/searcher.css";
 import "../styles/playground.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import { ReactElement, ReactNode } from "react";
 import { NextPage } from "next";
-
 import NextNProgress from "nextjs-progressbar";
-import Link from "next/link";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -22,10 +21,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   return (
     <>
       <NextNProgress color="#0069FF" />
-      <ThemeProvider defaultTheme="dark">
-        {getLayout(<Component {...pageProps} />)}
-      </ThemeProvider>
+      <ThemeProvider>{getLayout(<Component {...pageProps} />)}</ThemeProvider>
     </>
   );
 }
-//
