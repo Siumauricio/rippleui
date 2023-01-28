d6a8bda61326cee0e93a5c6f3256b9ecb94fcc75
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2023-01-28 00:46:45 -0600

    refactor(docs): update layout

--- docs/components/layout/DocsLayout.tsx
+++ docs/components/layout/DocsLayout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Headings } from "../../utils/getToc";
-import { Navbar } from "../styles/Navbar";
+import { NavbarDocs } from "../styles/NavbarDocs";
 import { SideNav } from "../styles/SideNav";
 import { TableOfContents } from "../styles/TableOfContents";
 
@@ -11,22 +11,29 @@ interface Props {
 
 export const DocsLayout = ({ children, headings }: Props) => {
   return (
-    <>
-      <div className="page sticky top-16 z-[2] mx-auto flex w-full max-w-[1390px] flex-grow">
-        <Navbar className="bg-backgroundPrimary/70" />
+    <div className="flex h-screen w-full flex-col">
+      <SideNav />
 
-        <div className="hidden lg:block">
-          <SideNav />
-        </div>
-        <main className="top-16 h-full w-full flex-grow overflow-hidden p-[4.5rem_1.5rem_2.5rem]  text-base hide-scrollbar lg:p-[7rem_6rem_2rem]">
-          {children}
+      <div className="div">
+        <NavbarDocs />
+        <main className="content lg:pl-[272px] 2xl:pl-[calc((100vw-var(--vp-layout-max-width))/2+var(--vp-sidebar-width))] 2xl:pr-[calc((100vw-var(--vp-layout-max-width))/2)]">
+          <div className="w-full pt-8 lg:p-[32px_32px_0]">
+            <div className=" flex w-full">
+              <div className="relative m-0  w-full overflow-hidden px-6 sm:min-w-[640px] lg:p-[0_32px_128px]">
+                <div className="mx-auto lg:max-w-[43rem]">{children}</div>
+              </div>
+              <div className="relative hidden w-full max-w-[256px] flex-grow pl-8 xl:block">
+                <div className="fixed bottom-0 z-10 h-8 w-56 " />
+                <div className="sticky top-0 -mt-28 h-screen pt-28">
+                  {headings && headings.length > 0 && (
+                    <TableOfContents toc={headings} />
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
         </main>
-        <div className="hidden lg:block">
-          {headings && headings.length > 0 && (
-            <TableOfContents toc={headings} />
-          )}
-        </div>
       </div>
-    </>
+    </div>
   );
 };
