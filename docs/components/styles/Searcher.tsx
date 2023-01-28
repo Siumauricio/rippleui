5aca74c1d52c66f05dc292d3dbfbd730c728e045
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2023-01-28 00:53:05 -0600

    feat(docs): add algolia searcher

--- docs/components/styles/Searcher.tsx
+++ docs/components/styles/Searcher.tsx
@@ -1,200 +1,87 @@
-import clsx from "clsx";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import React, { useEffect, useRef, useState } from "react";
-import { useOutsideAlerter } from "../../hooks/useOutsideAlerter";
+import Router from "next/router";
+import React, { useCallback, useEffect, useState } from "react";
+import { DocSearchModal, useDocSearchKeyboardEvents } from "@docsearch/react";
+import type {
+  InternalDocSearchHit,
+  StoredDocSearchHit,
+} from "@docsearch/react/dist/esm/types";
+import { createPortal } from "react-dom";
 import { SearchIcon } from "../icons/SearchIcon";
 
-interface IComponent {
-  name: string;
-  link: string;
+const docSearchConfig = {
+  appId: process.env.NEXT_PUBLIC_DOCSEARCH_APP_ID || "",
+  apiKey: process.env.NEXT_PUBLIC_DOCSEARCH_API_KEY || "",
+  indexName: process.env.NEXT_PUBLIC_DOCSEARCH_INDEX_NAME || "",
+};
+
+interface HitProps {
+  hit: InternalDocSearchHit | StoredDocSearchHit;
+  children: React.ReactNode;
 }
 
-const Components: IComponent[] = [
-  {
-    name: "Installation",
-    link: "/docs/get-started/installation",
-  },
-  {
-    name: "Usage",
-    link: "/docs/get-started/usage",
-  },
-  {
-    name: "Theme",
-    link: "/docs/get-started/theme",
-  },
-  {
-    name: "Button",
-    link: "/docs/components/button",
-  },
-  {
-    name: "Card",
-    link: "/docs/components/card",
-  },
-  {
-    name: "Checkbox",
-    link: "/docs/components/checkbox",
-  },
-  {
-    name: "Dropdown",
-    link: "/docs/components/dropdown",
-  },
-  {
-    name: "Input",
-    link: "/docs/components/input",
-  },
-  {
-    name: "Modal",
-    link: "/docs/components/modal",
-  },
-  {
-    name: "Navbar",
-    link: "/docs/components/navbar",
-  },
-  {
-    name: "Drawer",
-    link: "/docs/components/drawer",
-  },
-  {
-    name: "Radio",
-    link: "/docs/components/radio",
-  },
-  {
-    name: "Select",
-    link: "/docs/components/select",
-  },
-  {
-    name: "Switch",
-    link: "/docs/components/switch",
-  },
-  {
-    name: "Table",
-    link: "/docs/components/table",
-  },
-  {
-    name: "Tooltip",
-    link: "/docs/components/tooltip",
-  },
-  {
-    name: "Button Group",
-    link: "/docs/components/button-group",
-  },
-  {
-    name: "Avatar",
-    link: "/docs/components/avatar",
-  },
-  {
-    name: "Badge",
-    link: "/docs/components/badge",
-  },
-  {
-    name: "Breadcrumb",
-    link: "/docs/components/breadcrumb",
-  },
-  {
-    name: "Alert",
-    link: "/docs/components/alert",
-  },
-  {
-    name: "Textarea",
-    link: "/docs/components/textarea",
-  },
-  {
-    name: "Tabs",
-    link: "/docs/components/tabs",
-  },
-  {
-    name: "Accordion",
-    link: "/docs/components/accordion",
-  },
-];
-
-interface Props {
-  isMobile?: boolean;
+function Hit({ hit, children }: HitProps) {
+  return <Link href={hit.url}>{children}</Link>;
 }
 
-export const Searcher = ({ isMobile = false }: Props) => {
-  const router = useRouter();
-  const [value, setValue] = useState("");
-  const wrapperRef = useRef(null);
-  useOutsideAlerter(wrapperRef);
-  const [suggestions, setSuggestions] = useState<IComponent[]>([]);
+export function Search() {
+  let [isOpen, setIsOpen] = useState(false);
+  let [modifierKey, setModifierKey] = useState("");
+
+  const onOpen = useCallback(() => {
+    setIsOpen(true);
+  }, [setIsOpen]);
+
+  const onClose = useCallback(() => {
+    setIsOpen(false);
+  }, [setIsOpen]);
+
+  useDocSearchKeyboardEvents({ isOpen, onOpen, onClose });
 
   useEffect(() => {
-    const handleEsc = (event: KeyboardEvent) => {
-      if (event.keyCode === 27) {
-        console.log("Close");
-      }
-    };
-    window.addEventListener("keydown", handleEsc);
-
-    return () => {
-      window.removeEventListener("keydown", handleEsc);
-    };
+    setModifierKey(
+      /(Mac|iPhone|iPod|iPad)/i.test(window.navigator.userAgent) ? "⌘" : "Ctrl "
+    );
   }, []);
 
-  useEffect(() => {
-    if (value) {
-      const filtered = Components.filter((component) => {
-        return component.name.toLowerCase().includes(value.toLowerCase());
-      });
-      setSuggestions(filtered);
-    }
-  }, [value]);
-
-  const onClick = (link: string) => {
-    router.push(link);
-    setValue("");
-  };
-
-  const onFocus = () => {
-    if (value.length > 0 && suggestions.length > 0 && wrapperRef.current) {
-      // @ts-ignore
-      wrapperRef.current.style.visibility = "visible";
-    }
-  };
-
   return (
-    <div
-      className={clsx(
-        !isMobile && "hidden lg:flex",
-        "relative items-center justify-between "
-      )}
-    >
-      <div className="max-w-[18rem]">
-        <div className="relative">
-          <div className="pointer-events-none absolute inset-y-0 left-0 top-0 flex items-center pl-2">
-            <SearchIcon />
-          </div>
-          <input
-            value={value}
-            onFocus={onFocus}
-            onChange={(e) => setValue(e.target.value)}
-            type="search"
-            className="input px-8"
-            autoComplete="off"
-            placeholder="Search"
-          />
-          {value.length > 0 && suggestions.length > 0 && (
-            <>
-              <div
-                ref={wrapperRef}
-                className="absolute z-10 mt-1 max-h-72 w-full divide-y divide-gray-500 overflow-y-auto rounded-lg border border-gray-500 bg-backgroundSecondary shadow"
-              >
-                {suggestions.map((component) => (
-                  <label
-                    key={component.name}
-                    className="block cursor-pointer p-2 hover:bg-gray-300"
-                    onClick={() => onClick(component.link)}
-                  >
-                    {component.name}
-                  </label>
-                ))}
+    <>
+      <button
+        type="button"
+        className="flex  h-5 w-5 items-center justify-start  hover:border-gray-600 md:h-auto md:w-64 md:flex-none md:rounded-lg  md:border md:border-gray-500 md:bg-backgroundSecondary md:py-2 md:pl-4 md:pr-3.5   md:text-sm"
+        onClick={onOpen}
+      >
+        <SearchIcon className="-ml-2 h-5 w-5 flex-none fill-content3 " />
+        <span className="sr-only text-content3 md:not-sr-only md:ml-2">
+          Search docs
+        </span>
+        {modifierKey && (
+          <kbd className="ml-auto hidden font-medium text-content3 md:block">
+            <kbd className="font-sans">{modifierKey}</kbd>
+            <kbd className="font-sans">K</kbd>
+          </kbd>
+        )}
+      </button>
+      {isOpen &&
+        createPortal(
+          <DocSearchModal
+            {...docSearchConfig}
+            resultsFooterComponent={() => (
+              <div>
+                <a href="https://www.algolia.com/docsearch">Algolia</a>
               </div>
-            </>
-          )}
-        </div>
-      </div>
-    </div>
+            )}
+            initialScrollY={window.scrollY}
+            onClose={onClose}
+            hitComponent={Hit}
+            navigator={{
+              navigate({ itemUrl }) {
+                Router.push(itemUrl);
+              },
+            }}
+          />,
+          document.body
+        )}
+    </>
   );
-};
+}
