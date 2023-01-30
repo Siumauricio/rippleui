480f08b8af54e30b1a1425baaf5dda71cad366ed
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2023-01-29 23:16:12 -0600

    feat(docs): add dots, kbd, progress and range component docs

--- docs/components/mdx/Code.tsx
+++ docs/components/mdx/Code.tsx
@@ -7,7 +7,8 @@ interface Props {
 }
 
 export const Code = ({ children, className, ...props }: Props) => {
-  if (!className) return <code className="text-primary">{children}</code>;
+  if (!className)
+    return <code className="text-sm text-primary">{children}</code>;
   return (
     <CodeBlock
       language={className.split("-")[1] as "html" | "tsx" | "js" | "bash"}
